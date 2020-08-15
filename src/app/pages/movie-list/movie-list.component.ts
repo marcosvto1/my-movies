db653589a592a941123eaf2cb5144f930e3e9e56
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-15 18:49:10 -0300

    add MovieListComponent

---
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Movie {
+  id: number;
+  title: string;
+  obs: string;
+  rating: number;
+  toWatchNext: boolean;
+}
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+
+  movies: Movie[] = [
+    {
+      id: 1,
+      title: 'Bastardos Inglório',
+      obs: 'La la la lal lal al la la, allsal',
+      rating: 5,
+      toWatchNext: true
+    },
+    {
+      id: 2,
+      title: 'As trança da vovo careca',
+      obs: 'as trança da vovo careca',
+      rating: 1,
+      toWatchNext: true
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
