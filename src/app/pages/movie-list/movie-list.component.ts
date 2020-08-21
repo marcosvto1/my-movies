256dd383a83fbb26b8011bb346d3227c865a69dd
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-21 11:10:09 -0300

    clear object movies

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -15,71 +15,7 @@ interface Movie {
 })
 export class MovieListComponent implements OnInit {
 
-  movies: Movie[] = [
-    {
-      id: 1,
-      title: 'Bastardos Inglório',
-      obs: 'La la la lal lal al la la, allsal',
-      rating: 5,
-      toWatchNext: false
-    },
-    {
-      id: 2,
-      title: 'As trança da vovo careca',
-      obs: 'as trança da vovo careca',
-      rating: 1,
-      toWatchNext: true
-    },
-    {
-      id: 3,
-      title: 'Vampiro',
-      obs: 'as trança da vovo careca',
-      rating: 1,
-      toWatchNext: false
-    },
-    {
-      id: 4,
-      title: 'Vampiro 1',
-      obs: 'as trança da vovo careca',
-      rating: 1,
-      toWatchNext: true
-    },
-    {
-      id: 5,
-      title: 'Vampiro 2',
-      obs: 'as trança da vovo careca',
-      rating: 1,
-      toWatchNext: true
-    },
-    {
-      id: 6,
-      title: 'Vampiro 3',
-      obs: 'as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca',
-      rating: 1,
-      toWatchNext: false
-    },
-    {
-      id: 6,
-      title: 'Vampiro 3',
-      obs: 'as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca',
-      rating: 1,
-      toWatchNext: false
-    },
-    {
-      id: 6,
-      title: 'Vampiro 3',
-      obs: 'as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca',
-      rating: 1,
-      toWatchNext: false
-    },
-    {
-      id: 6,
-      title: 'Vampiro 3',
-      obs: 'as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca as trança da vovo careca',
-      rating: 1,
-      toWatchNext: false
-    },
-  ];
+  movies: Movie[] = [];
 
   constructor() { }
 
