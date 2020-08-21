66146e43da470d4b34994e6ce259c074565be43b
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-21 11:08:46 -0300

    import api memory module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { AboutComponent } from '@src/app/pages/about/about.component';
 import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data-service';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {put204: false, dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
